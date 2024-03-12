22136eedd8f76ca1d944e1d0b25f5cb476f3a559
Robert <robertwt7@gmail.com>
2024-03-12 06:06:54 +0000

    fix: wrong color on homepage

--- app/layout.tsx
+++ app/layout.tsx
@@ -43,7 +43,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en" className="h-full bg-white">
+    <html lang="en" className="h-full">
       <head>
         <ColorSchemeScript />
       </head>
