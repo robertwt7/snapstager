96228b118eb47cde3838bea011278a64cd48ec3c
ydideh810 <yd.baller810@gmail.com>
2024-09-19 20:56:11 -0400

    Update theme.ts

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -2,7 +2,7 @@ import { createTheme } from "@mantine/core";
 
 export const theme = createTheme({
   primaryShade: 5,
-  primaryColor: "blueWhale",
+  primaryColor: "#122635",
   colors: {
     blueWhale: [
       "#838f9a",
