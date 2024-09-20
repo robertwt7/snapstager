7d4bf5eccb390067a25360a551bb5a14c1b09e7d
ydideh810 <yd.baller810@gmail.com>
2024-09-20 19:06:41 -0400

    Update faq.tsx

--- src/components/faq/faq.tsx
+++ src/components/faq/faq.tsx
@@ -3,19 +3,19 @@ const faqs = [
     id: 1,
     question: "Why does it takes longer for some images to generate?",
     answer:
-      "GPU that is used at Replicate has Cold Boot time. It takes longer for the first image to generate. After the first image is generated, the next images will be generated faster. In the future we might rent a dedicated GPU to solve this problem once we have more users",
+      "The GPU that is used at Replicate has Cold Boot time. The first image takes longer to generate. After the first image is generated, the next images will be generated faster. In the future we might rent a dedicated GPU to solve this problem once we have more users",
   },
   {
     id: 2,
     question: "Who should I contact for support?",
-    answer: "Email me at robert@snapstager.com",
+    answer: "yonis@niddam.ai",
   },
 ];
 export const FAQ = () => {
   return (
     <div className="isolate mx-auto max-w-2xl divide-y divide-gray-900/10 px-6 pb-8 sm:pb-24 sm:pt-12 lg:max-w-7xl lg:px-8 lg:pb-32">
       <h2 className="text-2xl font-bold leading-10 tracking-tight text-gray-900">
-        Frequently asked questions
+        F A Q
       </h2>
       <dl className="mt-10 space-y-8 divide-y divide-gray-900/10">
         {faqs.map((faq) => (
