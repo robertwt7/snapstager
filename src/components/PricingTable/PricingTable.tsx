2e715628612ebbfa6d224fa3a5a8cdb76f517674
ydideh810 <yd.baller810@gmail.com>
2024-09-20 23:53:28 -0400

    Update PricingTable.tsx

--- src/components/PricingTable/PricingTable.tsx
+++ src/components/PricingTable/PricingTable.tsx
@@ -17,7 +17,7 @@ export const PricingTable = () => {
     <>
       <div className="mt-4 mb-8 flex w-full flex-1 flex-col items-center justify-center px-4 text-center sm:mb-0">
         <h1 className="font-display mx-auto mb-5 max-w-4xl text-4xl font-bold tracking-normal md:text-6xl text-blue-700">
-          Buy credits to use SpaceShift
+          BUY CREDITS!
         </h1>
         <div className="mt-8 w-full">
           <stripe-pricing-table
